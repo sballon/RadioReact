2cac83faab6af87e9775595ff98e5ab50e51b49e
ObedGS <jogs.12345@gmail.com>
2021-12-08 18:43:28 -0700

    Finish

--- App.js
+++ App.js
@@ -1,83 +1,183 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, Image , View, Button, NativeModules, TouchableOpacity, Pressable } from 'react-native';
-import Slider from '@react-native-community/slider';
-import { useFonts } from 'expo-font';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  Button,
+  NativeModules,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+import Slider from "@react-native-community/slider";
+import { useFonts } from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import Inicio from "./Inicio";
+import List from "./List";
+
+import img1 from "./assets/LogoZeta.png";
+import img2 from "./assets/eb9ff1bc02ac9172812d091d341801d3.jpg";
+import img3 from "./assets/s3066g.png";
+import img4 from "./assets/logog.png";
+import img5 from "./assets/download.jpg";
+import imunlike from "./assets/like.png"
+import imliked from "./assets/liked.png"
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [loaded] = useFonts({
-    Roboto: require('./assets/Fonts/Roboto-Bold.ttf'),
+    Roboto: require("./assets/Fonts/Roboto-Bold.ttf"),
   });
-  
+
   if (!loaded) {
     return null;
   }
   return (
-    
-    <View style={styles.container}>
-      <Pressable style={styles.backbutton} onPress={() => alert('Return')}>
-      <Image  source={require('./assets/back.png')} style={styles.image2}/>
-      </Pressable>
-      <Pressable style={styles.button1} onPress={() => alert('Logo')}>
-      <Image source={require('./assets/LogoZeta.png')} style={styles.image1}/>
-      </Pressable>
-      <Text style={{ fontFamily: 'Roboto', fontSize: 30, color: 'white', fontWeight: 'bold', bottom:"5%",}}>93.9</Text>
-      <View style={styles.fixToText}>
-      <Pressable onPress={() => alert('Previous')}>
-      <Image style={styles.Scenebutton1} source={require('./assets/izquierda.png')} />
-      </Pressable>
-      <Pressable onPress={() => alert('Next')}>
-      <Image style={styles.Scenebutton2} source={require('./assets/derecha.png')} />
-      </Pressable>
-      </View>
-      <View style={styles.optionsPad}>
-      <Pressable onPress={() => alert('List')}>
-      <Image style={styles.Optionbutton1} source={require('./assets/lista.png')} />
-      </Pressable>
-      <Pressable onPress={() => alert('Liked')}>
-      <Image style={styles.Optionbutton2} source={require('./assets/like.png')} />
-      </Pressable>
-      </View>
-      <View style={styles.slidePad}>
-      <Pressable onPress={() => alert('Volume down')}>
-      <Image style={styles.image2} source={require('./assets/quiet.png')} />
-      </Pressable>
-      <StatusBar style="auto"/>
-      <Slider
-        style={{width: 200, height: 50}}
-        minimumValue={0}
-        maximumValue={1}
-        minimumTrackTintColor="#FFFFFF"
-        maximumTrackTintColor="#000000"
-        thumbTintColor="#FFFFFF"
-      />
-      <Pressable onPress={() => alert('Volume up')}>
-      <Image style={styles.image2} source={require('./assets/loud.png')} />
-      </Pressable>
-      </View>
-      
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Inicio"
+          component={Inicio}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="List"
+          component={List}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
+const Home = ({ navigation }) => {
+  var [count, setCount] = useState(1);
+  var stations = [93.9, 95.5, 96.3, 99.3, 100.3];
+  var images = [img1, img2 ,img3, img4, img5];
+  var [station, setStation] = useState(stations[0]);
+  var [image, setImage] = useState(images[0]);
+  var [corazon, setcorazon] = useState(imunlike);
+  var [liked, setliked] = useState(false);
 
+
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.backbutton} onPress={() => navigation.goBack()}>
+        <Image source={require("./assets/back.png")} style={styles.image2} />
+      </Pressable>
+      <View style={styles.button1}>
+        <Image source={image} style={styles.image1} />
+      </View>
+      <Text
+        style={{
+          fontFamily: "Roboto",
+          fontSize: 30,
+          color: "white",
+          fontWeight: "bold",
+          bottom: "5%",
+        }}
+      >
+        {station}
+      </Text>
+      <View style={styles.fixToText}>
+        <Pressable style={styles.acomodo1}
+          onPress={() => {
+            setCount(count - 1);
+            setStation(stations[count % stations.length]);
+            setImage(images[count % images.length]);
+          }}
+        >
+          <Image
+            style={styles.Scenebutton1}
+            source={require("./assets/izquierda.png")}
+          />
+        </Pressable>
+        <Pressable style={styles.acomodo2}
+          onPress={() => {
+            setCount(count + 1);
+            setStation(stations[count % stations.length]);
+            setImage(images[count % images.length]);
+          }}
+        >
+          <Image
+            style={styles.Scenebutton2}
+            source={require("./assets/derecha.png")}
+          />
+        </Pressable>
+      </View>
+      <View style={styles.optionsPad}>
+        <Pressable onPress={() => navigation.navigate("List")}>
+          <Image
+            style={styles.Optionbutton1}
+            source={require("./assets/lista.png")}
+          />
+        </Pressable>
+        <Pressable onPress={() =>{
+          if(liked){
+            setliked(false);
+            setcorazon(imunlike);
+          }else{
+            setliked(true);
+            setcorazon(imliked);
+          }
+        }}>
+          <Image
+            style={styles.Optionbutton2}
+            source={corazon}
+          />
+        </Pressable>
+      </View>
+      <View style={styles.slidePad}>
+        <Pressable onPress={() => alert("Volume down")}>
+          <Image
+            style={styles.image22}
+            source={require("./assets/quiet.png")}
+          />
+        </Pressable>
+        <StatusBar style="auto" />
+        <Slider
+          style={{ width: 250, height: 50 }}
+          minimumValue={0}
+          maximumValue={1}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+          thumbTintColor="#FFFFFF"
+        />
+        <Pressable onPress={() => alert("Volume up")}>
+          <Image style={styles.image22} source={require("./assets/loud.png")} />
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000099',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+    backgroundColor: "#000099",
+    alignItems: "center",
+    justifyContent: "center",
   },
   baseText: {
-    fontWeight: 'bold',
-    
+    fontWeight: "bold",
   },
   innerText: {
-    color: 'white',
+    color: "white",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
 
     padding: 100,
   },
@@ -97,41 +197,52 @@ const styles = StyleSheet.create({
   image1: {
     width: 170,
     height: 170,
-    borderRadius:100,
+    borderRadius: 100,
   },
   image2: {
-    width: 48,
-    height: 48,
+    width: 30,
+    height: 30,
+  },
+  image22: {
+    width: 30,
+    height: 30,
+    top: 10,
   },
   button1: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: 175,
     height: 175,
-    borderRadius:100,
-    backgroundColor: 'white',
+    borderRadius: 100,
+    backgroundColor: "white",
     bottom: "10%",
   },
   fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     width: "30%",
   },
   Scenebutton1: {
     width: 48,
     height: 48,
     bottom: "40%",
-    right: "50%",
+    
   },
   Scenebutton2: {
     width: 48,
     height: 48,
     bottom: "40%",
-    left: "50%",
+    
   },
-  optionsPad:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  acomodo1:{
+    right: "50%"
+  },
+  acomodo2:{
+    left: "50%"
+  },
+  optionsPad: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     top: "10%",
     width: "60%",
   },
@@ -147,9 +258,9 @@ const styles = StyleSheet.create({
     bottom: "0%",
     left: "20%",
   },
-  slidePad:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  slidePad: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     top: "20%",
-  }
+  },
 });
